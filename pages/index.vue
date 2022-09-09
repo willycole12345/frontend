4696cb13d07822f83019ae78d9c878ddafa43e1d
Your Name <you@example.com>
2022-09-09 03:46:37 +0100

    indexpage changed

--- pages/index.vue
+++ pages/index.vue
@@ -4,49 +4,47 @@
       <div class="m-4">
         <div class="container text-center">
           <div class="col-sm-6">
+            <h2 class="text-left"> Shape </h2>
             <div class="row row-cols-auto">
-              <div class="col-sm-2">
-                <div class="col dot"></div>
+              <div v-for="shape in shapes">
+              <div class="col-sm-2" style="cursor: pointer;">
+                <div @click=" getshape(shape.namesofshape)">{{ shape.namesofshape }}</div>
               </div>
-              <div class="col-sm-2">
-                <div class="col dot2"></div>
               </div>
-              <div class="col-sm-2">
-                <div class="col dot3"></div>
+            </div>
+            </div>
+            <br/>
+            
+          <div class="col-sm-6">
+            <h2 class="text-left">Color </h2>
+            <div class="row row-cols-auto">
+            
+              <div v-for="shape in shapes">
+              <div class="col-sm-2" style="cursor: pointer;"  >
+                <div v-bind:class="getColor(shape.name)"></div>
               </div>
+              </div>
+            </div>
+            </div>
           </div>
     </div>
   </div>
     <div class="container text-center">
       <div class="m-4">
         <div class="row">
-          <div class="col-sm-4">
-              <div class="card" style="width: 18rem;">
-                  <div class="card-body">
-                  <div class="dot"></div>
-                  </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="card" style="width: 18rem;">
-                  <div class="card-body">
-                  <div class="dot2"></div>
-                  </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="card" style="width: 18rem;">
-                  <div class="card-body">
-                  <div class="dot3"></div>
-                  </div>
+          <div v-for="shape in shapes">
+              <div class="col-sm-4">
+                  <div class="card" style="width: 18rem;">
+                      <div class="card-body">
+                      <div v-bind:class="getColor(shape.name)"></div>
+                      </div>
+                    </div>
                 </div>
             </div>
           </div> 
         </div>
       </div>
       </div>
-    </div>
-  </div>
 </template>
 <style>
 .dot {
@@ -70,6 +68,27 @@
   border-radius: 50%;
   display: inline-block;
 }
+.dot4{
+  height: 25px;
+  width: 25px;
+  background-color: #fdf906;
+  border-radius: 50%;
+  display: inline-block;
+}
+.dot5 {
+  height: 25px;
+  width: 25px;
+  background-color: #515800;
+  border-radius: 50%;
+  display: inline-block;
+}
+.dot6 {
+  height: 25px;
+  width: 25px;
+  background-color: #000000;
+  border-radius: 50%;
+  display: inline-block;
+}
 .content{
   width: 5%;
   height: 40px;
@@ -81,21 +100,63 @@
 export default {
     name: "IndexPage",
   //  components: { Header }
-  data: () => ({
-        posts: []
-      }),
-      async fetch() {
-        this.posts = await this.$http.$get('https://api.nuxtjs.dev/posts')
+  data() {
+    sortKey: ['name','namesofshape']
+    columns: ['name', 'smallshape','namesofshape']
+  return {
+    shapes: [
+      {
+        name:'red',
+        smallshape: 'red',
+        namesofshape:'cube'
+       },    
+       {
+        name:'Green',
+        smallshape: 'Green',
+        namesofshape:'box'
+       },   
+       {
+        name:'blue',
+        smallshape:'blue',
+        namesofshape:'triangle'
+       } ,   
+       {
+        name:'yellow',
+        smallshape: 'yellow', 
+        namesofshape:'round'
+       },   
+       {
+        name:'orange',
+        smallshape:'orange',
+        namesofshape:'circle'
+       } ,  
+       {
+        name:'black',
+        smallshape: 'black',
+        namesofshape:'rectangle'
+       }    
+      ],
+      color:{
+              'red':'dot',
+              'Green':'dot2',
+              'blue':'dot3',
+              'yellow':'dot4',
+              'orange':'dot5',
+              'black':'dot6',
+            }
+        }
       },
-      fetchOnServer: false,
-      // multiple components can return the same `fetchKey` and Nuxt will track them both separately
-      fetchKey: 'site-sidebar',
-      // alternatively, for more control, a function can be passed with access to the component instance
-      // It will be called in `created` and must not depend on fetched data
-      fetchKey(getCounter) {
-        // getCounter is a method that can be called to get the next number in a sequence
-        // as part of generating a unique fetchKey.
-        return this.someOtherData + getCounter('sidebar')
-      }
-}
+      methods: {
+          getColor(name) {
+            console.log(name);
+            return this.color[name]
+          },
+          getshape(namesofshape) {
+            return this.color[namesofshape]
+          },
+          
+        }
+    }
+
+  
 </script>
